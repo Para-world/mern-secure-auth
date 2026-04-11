edaeb25be2c561e807005f85db6e8998428246c9
Deepak kumar <dkumar39223@gmail.com>
2026-04-11 21:55:13 +0530

    api-testing

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -47,3 +47,16 @@ export async function register(req, res) {
 
 
 }
+
+export async function getMe(req, res) {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token){
+        return res.status(401).json({
+            message: "token not found"
+        })
+    }
+
+    const decoded = jwt.verify(token, config.jwt_secret);
+    console.log(decoded);
+    
+}
